f6daa8987a91c74489a5adc42340e08ddb762e25
Victor GRENU <victor.grenu@gmail.com>
2021-04-10 21:45:25 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ setup(
     author = 'Victor GRENU',                   # Type in your name
     author_email = 'vgrenu@zoph.io',      # Type in your E-Mail
     url = 'https://github.com/z0ph/aa-policy-validator',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['AWS', 'IAM', 'AccessAnalyzer'],   # Keywords that define your package best
+    download_url = 'https://github.com/z0ph/aa-policy-validator/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+    keywords = ['AWS', 'IAM', 'AccessAnalyzer', 'PolicyValidation'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
-            'validators',
-            'beautifulsoup4',
+            'boto3',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
